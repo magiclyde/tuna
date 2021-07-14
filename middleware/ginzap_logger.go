a0830d344810d2a41e46bd3c6fe259dcc2546afb
clyde <chenqiunuan@hnguangyi.cn>
2021-07-14 16:48:07 +0800

    log req & resp body

--- middleware/ginzap_logger.go
+++ middleware/ginzap_logger.go
@@ -9,14 +9,37 @@
 package middleware
 
 import (
+	"bytes"
 	"github.com/gin-gonic/gin"
 	"github.com/syyongx/php2go"
 	"go.uber.org/zap"
+	"io/ioutil"
 	"time"
 )
 
+type responseWriter struct {
+	gin.ResponseWriter
+	body *bytes.Buffer
+}
+
+func (w responseWriter) Write(b []byte) (int, error) {
+	w.body.Write(b)
+	return w.ResponseWriter.Write(b)
+}
+
+func (w responseWriter) WriteString(s string) (int, error) {
+	w.body.WriteString(s)
+	return w.ResponseWriter.WriteString(s)
+}
+
 func GinZapLogger(logger *zap.Logger, skipPaths []string) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		respWriter := &responseWriter{
+			body:           bytes.NewBufferString(""),
+			ResponseWriter: c.Writer,
+		}
+		c.Writer = respWriter
+
 		start := time.Now()
 		path := c.Request.URL.Path
 		query := c.Request.URL.RawQuery
@@ -29,6 +52,10 @@ func GinZapLogger(logger *zap.Logger, skipPaths []string) gin.HandlerFunc {
 		end := time.Now()
 		latency := end.Sub(start)
 
+		reqId := c.Request.Header.Get("X-Request-Id")
+		reqBody, _ := ioutil.ReadAll(c.Request.Body)
+		respBody := respWriter.body.String()
+
 		logger.Info(path,
 			zap.Int("status", c.Writer.Status()),
 			zap.String("method", c.Request.Method),
@@ -37,6 +64,9 @@ func GinZapLogger(logger *zap.Logger, skipPaths []string) gin.HandlerFunc {
 			zap.String("ip", c.ClientIP()),
 			zap.String("user-agent", c.Request.UserAgent()),
 			zap.Duration("latency", latency),
+			zap.String("reqId", reqId),
+			zap.ByteString("reqBody", reqBody),
+			zap.String("respBody", respBody),
 		)
 	}
 }
